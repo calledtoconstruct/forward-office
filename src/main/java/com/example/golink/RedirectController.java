f9a8b84b7dacdccc1b61f1c434e6d5c12c34e196
Kendyl Hull <hullkendyl@gmail.com>
2024-07-28 00:31:01 -0400

    removing api/vi from basepath

--- src/main/java/com/example/golink/RedirectController.java
+++ src/main/java/com/example/golink/RedirectController.java
@@ -9,7 +9,7 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping()
 public class RedirectController {
 
     @Autowired
